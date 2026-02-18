f705688c37bbf6a1328d1605eb0b016a7be744fc
Subhas Hegde <subhashegde68@gmail.com>
2026-02-18 21:38:34 +0530

    Problems

--- AddStrings.java
+++ AddStrings.java
@@ -2,8 +2,8 @@ public class AddStrings {
     public static String addStrings(String num1, String num2) {
         int n1=Integer.parseInt(num1);
         int n2=Integer.parseInt(num2);
-        Integer ans=n1+n2;
-        return ans.toString();
+        int ans=n1+n2;
+        return Integer.toString(ans);
     }
 
     public static void main(String[] args) {
