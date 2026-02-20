f3f476cfc93f2a3b3f0a180187c27af5b25bae39
Subhas Hegde <subhashegde68@gmail.com>
2026-02-20 09:19:19 +0530

    PalindromeString

---
+++ PalindromeString.java
@@ -0,0 +1,18 @@
+public class PalindromeString {
+    public static boolean isplaindrome(String str){
+        int start=0;
+        int end=str.length()-1;
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str="racecar";
+        System.out.println(isplaindrome(str));
+    }
+}
