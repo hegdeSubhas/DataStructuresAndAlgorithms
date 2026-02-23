8cd7a869c7b3e6f313a4bb0db417f28dd3ae05e6
Subhas Hegde <subhashegde68@gmail.com>
2026-02-23 21:39:19 +0530

    SelectionSort

---
+++ SelectionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={23,54,87,12,1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selectionSort(int[] arr){
+        int n= arr.length;
+        for (int i=0;i<n-1;i++) {
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
